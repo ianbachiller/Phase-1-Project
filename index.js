f8ee072daf5b1202464a8b43c1842c4eef42ae2e
Ian Bachiller <ianbachise@gmail.com>
2024-02-29 03:43:08 -0500

    working show all procedures feature; click event

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
-
-
-
-
-
-
-
-
-    
+//Click event to show all procedures
+const showAllProceduresNamesBtn = document.getElementById("all-procedures");
+showAllProceduresNamesBtn.addEventListener("click", fetchAllProcedures);
+  function fetchAllProcedures() {
+    fetch("http://localhost:3000/procedures")
+      .then((resp) => resp.json())
+      .then((procedures) => iterateProcedureNames(procedures))
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }
+  const procCont = document.getElementById("all-procedures-container");
+    function iterateProcedureNames(icuProcedures) {
+    //   selectedProcedureDiv.innerHTML = " "
+      icuProcedures.forEach((procedure) => {
+        const p = document.createElement("p");
+        // p.addEventListener("dblclick", handleDoubleCLick);
+        p.textContent = procedure.name;
+        p.setAttribute("id", procedure.id);
+        procCont.append(p);
+      });
+    }
+//Click event to show all procedures
 })
